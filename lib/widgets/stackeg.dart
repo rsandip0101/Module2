3b347671a089f2863bf16675970fbd0a8c65600d
R Sandip <rsandip.0101@gmail.com>
2026-01-28 13:00:09 +0530

    stackeg

---
+++ lib/widgets/stackeg.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class Stackeg extends StatefulWidget {
+  const Stackeg({super.key});
+
+  @override
+  State<Stackeg> createState() => _StackegState();
+}
+
+class _StackegState extends State<Stackeg> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.blue,
+      ),
+    );
+  }
+}
